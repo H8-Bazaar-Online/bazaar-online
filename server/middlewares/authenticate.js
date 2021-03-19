8b3b95d4b661484deee9ba361cf88a8bbedae019
dodytrifa <dody.trifa@gmail.com>
2021-03-19 13:35:48 +0700

    add server-user

---
+++ server/middlewares/authenticate.js
@@ -0,0 +1,31 @@
+const { verify } = require('../helpers/jwt')
+const { User } = require('../models')
+
+function authenticate(req, res, next) {
+  let access_token = req.headers.access_token
+
+  try {
+    const decoded = verify(access_token)
+
+    User.findOne({
+      where: {
+        id: decoded.id
+      }
+    })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({
+            message: "You are not authenticate"
+          })
+        } else {
+          req.decoded = decoded
+        }
+        next()
+      })
+
+  } catch (err) {
+    res.status(401).json(err)
+  }
+}
+
+module.exports = { authenticate }
---
+++ server/middlewares/authorize.js
@@ -0,0 +1,25 @@
+const { User } = require('../models/')
+
+const authorize = function (req, res, next) {
+  User.findOne({
+    where: {
+      email: req.decoded.email
+    }
+  })
+    .then(user => {
+      console.log(user.role);
+      if (user.role === "merchant") {
+        next()
+      } else {
+        res.status(401).json({ message: "You are not authorize" })
+        // next()
+      }
+    })
+    .catch(err => {
+      // console.log(err);
+      res.status(500).json(err)
+      // next(err)
+    })
+}
+
+module.exports = authorize
