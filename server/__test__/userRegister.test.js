a1e740dfdb013a2d3aa31ea0b6e66b4cecb6f54e
ahmad-akbar <ahmd.akbr25@gmail.com>
2021-03-19 15:01:03 +0800

    add tdd user for login and register

---
+++ server/__test__/userRegister.test.js
@@ -0,0 +1,62 @@
+const request = require('supertest')
+const app = require('../app.js')
+
+const userValid = { username: 'Merchant', email: 'admin.merchant@mail.com', password: 'admin123', role: 'merchant' }
+const userFieldEmpty = { username: '', email: '', password: '', role: '' }
+const userWrongTypeData = { username: 123, email: true, password: 'admin123', role: 'admin' }
+
+
+describe('POST /register success', () => {
+  test('register success', (done) => {
+    request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send(userValid)
+      .then(response => {
+        const { status, body } = response
+        expect(status).toBe(201)
+        expect(body).toHaveProperty('id', expect.any(Number))
+        expect(body).toHaveProperty('email', expect.any(String))
+        expect(body).toHaveProperty('username', expect.any(String))
+        expect(body).toHaveProperty('role', expect.any(String))
+        return done()
+      })
+      .catch(err => {
+        return done(err)
+      })
+  })
+})
+
+describe('POST /register failed', () => {
+  test('register failed, field empty', (done) => {
+    request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send(userFieldEmpty)
+      .then(response => {
+        const { status, body } = response
+        expect(status).toBe(400)
+        expect(Array.isArray(body)).toEqual(true)
+        return done()
+      })
+      .catch(err => {
+        return done(err)
+      })
+  })
+
+  test('register failed,  type data is wrong', (done) => {
+    request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send(userWrongTypeData)
+      .then(response => {
+        const { status, body } = response
+        expect(status).toBe(400)
+        expect(Array.isArray(body)).toEqual(true)
+        return done()
+      })
+      .catch(err => {
+        return done(err)
+      })
+  })
+})
