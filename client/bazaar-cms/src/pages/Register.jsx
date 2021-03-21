b2dcf97845913cf38ecfa23ada2e5eb9b43ce91f
Ramadhan Nur Cholis Januar <adhann98@gmail.com>
2021-03-21 10:18:13 +0700

    Add Template Register init

---
+++ client/bazaar-cms/src/pages/Register.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { login } from '../store/action';
+export default function Register() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value ,'<<<<<<<<<<<<<<< FORM DATA');
+    setFormData((prev) => ({ ...prev, [name]: value }))
+  }
+  
+  const dispatch = useDispatch()
+
+  const history = useHistory()
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    dispatch(login(formData))
+    // if (localStorage.access_token === 'undefined' || !localStorage.access_token) {
+    //   history.push('/login')
+    // } else {
+    //   history.push('/')
+    // }
+  }
+  
+  useEffect(() => {
+    // if (localStorage.access_token === 'undefined' || !localStorage.access_token) {
+    //   history.push('/login')
+    // } else {
+    //   history.push('/')
+    // }
+  })
+
+
+  return (
+    <div className="font-sans">
+      <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-900 ">
+        <div className="relative sm:max-w-sm w-full">
+          <div className="card bg-red-400 shadow-lg w-full h-full rounded-3xl absolute  transform -rotate-6">
+          </div>
+          <div className="card bg-yellow-400 shadow-lg w-full h-full rounded-3xl absolute  transform rotate-6">
+          </div>
+          <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
+            <label className="block text-sm text-gray-700 text-center font-semibold">
+              Register
+            </label>
+            <form onSubmit={handleOnSubmit} className="mt-10">
+              <div className="mt-7">                
+                <input value={formData.username} onChange={handleOnChange} name="username" type="text" placeholder="Username" className="mt-1 pl-3 py-3 block w-full border focus:border-yellow-500 bg-gray-100 h-11 rounded-xl focus:outline-none shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 placeholder-gray-500 placeholder-opacity-30"/>                           
+              </div>
+              <div className="mt-7">                
+                <input value={formData.email} onChange={handleOnChange} name="email" type="email" placeholder="Email" className="mt-1 pl-3 py-3 block w-full border focus:border-yellow-500 bg-gray-100 h-11 rounded-xl focus:outline-none shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 placeholder-gray-500 placeholder-opacity-30"/>                           
+              </div>
+              <div className="mt-7">                
+                <input value={formData.password} onChange={handleOnChange} name="password" type="password" placeholder="*******" className="mt-1 pl-3 py-3 block w-full border focus:border-yellow-500 bg-gray-100 h-11 rounded-xl focus:outline-none shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 placeholder-gray-500 placeholder-opacity-30"/>                           
+              </div>
+              <div className="mt-7">                
+                <input value={formData.name} onChange={handleOnChange} name="name" type="text" placeholder="Name" className="mt-1 pl-3 py-3 block w-full border focus:border-yellow-500 bg-gray-100 h-11 rounded-xl focus:outline-none shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 placeholder-gray-500 placeholder-opacity-30"/>                           
+              </div>
+              <div className="mt-7">                
+                <input onChange={handleOnChange} name="logo" type="file" placeholder="Email" className="mt-1 pl-3 px-3 pt-2 block w-full border focus:border-yellow-500 bg-gray-100 h-11 rounded-xl focus:outline-none shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 placeholder-gray-500 placeholder-opacity-30"/>                           
+              </div>
+              <div className="mt-7">                
+                <input value={formData.category} onChange={handleOnChange} name="category" type="text" placeholder="Category" className="mt-1 pl-3 py-3 block w-full border focus:border-yellow-500 bg-gray-100 h-11 rounded-xl focus:outline-none shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 placeholder-gray-500 placeholder-opacity-30"/>                           
+              </div>
+              <div className="mt-7">
+                <button className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                    Register
+                </button>
+              </div>
+              <div className="flex mt-7 items-center text-center">
+                <hr className="border-gray-300 border-1 w-full rounded-md"/>
+              </div>
+  
+              <div className="mt-7">
+                <div className="flex items-center text-center">
+                  <label className="w-full text-sm text-gray-600">Already have an account? </label>
+                  <a href="/#" className="w-full text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                    Login
+                  </a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
